ce3e191898a7261da6c2532c8c2fbf1023592c1d
Santiago <santiagofonsecaf311@gmail.com>
2023-10-01 11:32:10 -0500

    main

--- src/App/store.jsx
+++ src/App/store.jsx
@@ -1,8 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer } from 'redux-persist';
+import reduxPersistConfig from './reduxPersistConfig';
 import contactsReducer from '../Features/Contact/contactSlice';
+
+const persistedReducer = persistReducer(reduxPersistConfig, contactsReducer);
 
 export const store = configureStore({
     reducer: {
-        contacts: contactsReducer
+        contacts: persistedReducer
     }
 })
+
+export const persistor = persistStore(store);
